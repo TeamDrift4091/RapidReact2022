d0e087aa60a1e0d9ffd27c71262c0482b84d448d
StephenDay1 <57306301+StephenDay1@users.noreply.github.com>
2022-02-15 14:18:11 -0400

    Cleanup

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,12 +9,9 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.autonomous.Autonomous1Ball;
-import frc.robot.commands.autonomous.AutonomousTrajectory;
 import frc.robot.commands.drivetrain.JoystickDrive;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -26,8 +23,6 @@ public class RobotContainer {
 
   // The robot's controllers...
   private static Joystick joystick = new Joystick(0);
-
-  private static JoystickButton button1 = new JoystickButton(joystick,1);
 
   // The robot's subsystems and commands are defined here...
   private final Drivetrain drivetrain = new Drivetrain();
@@ -51,9 +46,7 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {
-    button1.whileHeld(new InstantCommand(()->{drivetrain.resetEncoders();}));
-  }
+  private void configureButtonBindings() {}
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
--- src/main/java/frc/robot/commands/autonomous/AutonomousTrajectory.java
+++ src/main/java/frc/robot/commands/autonomous/AutonomousTrajectory.java
@@ -4,20 +4,10 @@
 
 package frc.robot.commands.autonomous;
 
-import java.util.List;
-
 import edu.wpi.first.math.controller.RamseteController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.Drivetrain;
@@ -59,35 +49,33 @@ public class AutonomousTrajectory extends CommandBase {
     
     ChassisSpeeds speeds = ramseteController.calculate(drivetrain.getPose(), reference);
 
-    var normalized_ramsete_speed = speeds.vxMetersPerSecond / Constants.MAX_SPEED;
-    var normalized_ramsete_rotation = -speeds.omegaRadiansPerSecond / Constants.MAX_ANGULAR_VELOCITY;
+    double normalized_ramsete_speed = speeds.vxMetersPerSecond / Constants.MAX_SPEED;
+    double normalized_ramsete_rotation = -speeds.omegaRadiansPerSecond / Constants.MAX_ANGULAR_VELOCITY;
 
     drivetrain.arcadeDrive(normalized_ramsete_speed, normalized_ramsete_rotation, false);
 
+    // var t_pose = reference.poseMeters;
+    // var t_x = t_pose.getX();
+    // var t_y = t_pose.getY();
+    // var t_rotation = t_pose.getRotation().getDegrees();
 
+    // var a_pose = drivetrain.getPose();
+    // var a_x = a_pose.getX();
+    // var a_y = a_pose.getY();
+    // var a_rotation = a_pose.getRotation().getDegrees();
 
-    var t_pose = reference.poseMeters;
-    var t_x = t_pose.getX();
-    var t_y = t_pose.getY();
-    var t_rotation = t_pose.getRotation().getDegrees();
+    // SmartDashboard.putNumber("Ramsete Speed - Normalized", normalized_ramsete_speed);
+    // SmartDashboard.putNumber("Ramsete Rot - Normalized", normalized_ramsete_rotation);
 
-    var a_pose = drivetrain.getPose();
-    var a_x = a_pose.getX();
-    var a_y = a_pose.getY();
-    var a_rotation = a_pose.getRotation().getDegrees();
+    // SmartDashboard.putNumber("Pose X - Trajectory", t_x);
+    // SmartDashboard.putNumber("Pose Y - Trajectory", t_y);
+    // SmartDashboard.putNumber("Pose R - Trajectory", t_rotation);
 
-    SmartDashboard.putNumber("Ramsete Speed - Normalized", normalized_ramsete_speed);
-    SmartDashboard.putNumber("Ramsete Rot - Normalized", normalized_ramsete_rotation);
+    // SmartDashboard.putNumber("Pose X - Actual", a_x);
+    // SmartDashboard.putNumber("Pose Y - Actual", a_y);
+    // SmartDashboard.putNumber("Pose R - Actual", a_rotation);
 
-    SmartDashboard.putNumber("Pose X - Trajectory", t_x);
-    SmartDashboard.putNumber("Pose Y - Trajectory", t_y);
-    SmartDashboard.putNumber("Pose R - Trajectory", t_rotation);
-
-    SmartDashboard.putNumber("Pose X - Actual", a_x);
-    SmartDashboard.putNumber("Pose Y - Actual", a_y);
-    SmartDashboard.putNumber("Pose R - Actual", a_rotation);
-
-    SmartDashboard.putNumber("Raw Gyro Angle", drivetrain.getGyroAngle());
+    // SmartDashboard.putNumber("Raw Gyro Angle", drivetrain.getGyroAngle());
   }
 
   // Called once the command ends or is interrupted.
